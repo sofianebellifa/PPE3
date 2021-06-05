3d02e65a6a42470e94077020a3e5530c08d6f79c
MrMamie <mr.mamie@gmail.com>
2021-06-05 23:21:48 +0200

    pour momo

--- ppe3_vrai/index.php
+++ ppe3_vrai/index.php
@@ -6,7 +6,7 @@ require('header.php');
 
 
     <article class = "index">
-        <h2>GEFOR</h2>
+        <h2 class="champ">GEFOR</h2>
         <h3 class="champ">Avec l'application Gefor, visualisez vos dossiers d'admissions en temps réel. </h3>
 
 
